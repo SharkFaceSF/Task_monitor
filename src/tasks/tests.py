3a4d4ae33d3b83fae1aa6ce53b66256dead19dee
SharkFace34 <SharkFaceSF34@gmail.com>
2026-05-16 04:16:28 +0300

    add: tests and flower

--- src/tasks/tests.py
+++ src/tasks/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from .models import Task
 
-# Create your tests here.
+@pytest.mark.django_db
+def test_create_task_api(client):
+    """Тест эндпоинта создания задачи"""
+    url = reverse('task-list')  # это даст /api/tasks/
+    data = {'url': 'https://google.com'}
+    response = client.post(url, data)
+    
+    assert response.status_code == 201
+    assert Task.objects.count() == 1
+    assert Task.objects.get().url == 'https://google.com'
